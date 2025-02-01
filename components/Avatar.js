6727bd0f9cc6b4ff93aea4bcf29e4179bcecd20c
awesomejsbuilder <awesomebuildswithjerry@gmail.com>
2025-02-01 11:27:13 +0100

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
-    <Image src={'/avatar3.png'} alt="" width={631} height={563} className="w-full h-full translate-z-0"/>
+    <Image src={'/avatar4.png'} alt="" width={631} height={563} className="w-full h-full translate-z-0"/>
   </div>;
 };
 
